714d59d466db5132a23452e741707e98727a9722
alisa <alisa.solodovnik@gmail.com>
2024-03-19 18:23:34 +0700

    add file

---
+++ main.py
@@ -0,0 +1,30 @@
+# Case-study #4: Diet meals
+# Developers: Solodovnik A., Soknyshev D., Kabanova M., Kim A.
+#
+
+cost = 0
+protein = 0
+leftover = 0
+
+PROTEIN_COST = 99
+FISH_COST = 314.15
+CHICKEN_ COST = 499.29
+
+def fish(x):
+    cost = FISH_COST * serving
+    return cost
+
+def chicken(x):
+    cost = CHICKEN_ COST * serving
+    return cost
+
+def broke(x):
+    leftover = budget - cost
+    return leftover > 0
+
+serving = int(input()) # local in ()
+budget = int(input()) # local in ()
+meal = int(input()) # local in ()
+match meal:
+    case 1:
+
