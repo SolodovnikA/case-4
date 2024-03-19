4655a55958d44e4020a088849eb55740fea0ef3f
alisa <alisa.solodovnik@gmail.com>
2024-03-19 21:32:49 +0700

    thinking

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cost = 0
 protein = 0
 leftover = 0
 
-PROTEIN_COST = 99
+BAR_COST = 99
 FISH_COST = 314.15
 CHICKEN_COST = 499.29
 
@@ -16,6 +16,7 @@ CHICKEN_COST = 499.29
 def fish(x):
     cost = FISH_COST * x
     return cost
+
 
 
 def chicken(x):
@@ -28,22 +29,22 @@ def broke(x, y):
     return leftover > 0
 
 
-def main_function(meal, budget):
+def main_function(meal, prtns, budget):
     match meal:
         case 1:
-            if broke(budget, cost): print(f'{ru.FOR_MEAL} {ru.OPTION1} {ru.EXPENSES} {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
-                      f'{(broke(budget, cost)) // PROTEIN_COST}')
-            else: print(f'{ru.LACK} {ru.ADD} {cost-budget} {ru.RUBLS}')
+            if broke(budget, fish(prtns)) == True: print(f'{ru.FOR_MEAL} {ru.OPTION1} {ru.EXPENSES} {fish(prtns)}'
+                                          f' {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
+                                          f' {int((budget-fish(prtns)) // BAR_COST)}')
+            else: print(f'{ru.LACK} {ru.ADD} {round(fish(prtns)-budget, 2)} {ru.RUBLS}')
         case 2:
-            if broke(budget, cost):  print(f'{ru.FOR_MEAL} {ru.OPTION2} {ru.EXPENSES} {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
-                      f'{(broke(budget, cost)) // PROTEIN_COST}')
-            else: print(f'{ru.LACK} {ru.ADD} {cost - budget} {ru.RUBLS}')
-        case _:
-            print(f'{ru.ERROR}')
+            if broke(budget, chicken(prtns)): print(f'{ru.FOR_MEAL} {ru.OPTION2} {ru.EXPENSES} {chicken(prtns)}'
+                                          f' {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
+                                          f' {int((budget-chicken(prtns)) // BAR_COST)}')
+            else: print(f'{ru.LACK} {ru.ADD} {round(chicken(prtns)-budget, 2)} {ru.RUBLS}')
 
 
 if __name__ == "__main__":
     meal = float(input(ru.QUESTION_1))  # local in ()
-    portions = float(input(ru.QUESTION_2))  # local in ()
+    prtns = float(input(ru.QUESTION_2))  # local in ()
     budget = float(input(ru.QUESTION_3))  # local in ()
-    main_function(meal, budget)
+    main_function(meal, prtns, budget)
