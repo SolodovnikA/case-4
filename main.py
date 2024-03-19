903685184c705400713dda29948da54da34b9276
liho-o <67860443+liho-o@users.noreply.github.com>
2024-03-19 20:46:08 +0700

    change

--- main.py
+++ main.py
@@ -31,13 +31,15 @@ def broke(x, y):
 def main_function(meal, portions, budget):
     match meal:
         case 1:
-            if broke(budget,cost) == True: print(f'{ru.EXPENSES} {meal} {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
-                  f' {(broke(budget,cost))//PROTEIN_COST}')
-            else: print(f'{ru.LACK} {ru.ADD}')
+            if broke(budget, cost):
+                print(f'{ru.EXPENSES} {meal} {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
+                      f'{(broke(budget, cost)) // PROTEIN_COST}')
+            else:
+                print(f'{ru.LACK} {ru.ADD}')
 
 
 if __name__ == "__main__":
-    meal = int(input(ru.QUESTION_1)) # local in ()
-    portions = int(input(ru.QUESTION_2)) # local in ()
-    budget = int(input(ru.QUESTION_3)) # local in ()
+    meal = int(input(ru.QUESTION_1))  # local in ()
+    portions = int(input(ru.QUESTION_2))  # local in ()
+    budget = int(input(ru.QUESTION_3))  # local in ()
     main_function(meal, portions, budget)
