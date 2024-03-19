dbef09c936dda91bd668ca9279f34a8430673264
liho-o <67860443+liho-o@users.noreply.github.com>
2024-03-19 20:41:26 +0700

    change

--- main.py
+++ main.py
@@ -17,20 +17,27 @@ def fish(x):
     cost = FISH_COST * x
     return cost
 
+
 def chicken(x):
     cost = CHICKEN_COST * x
     return cost
+
 
 def broke(x, y):
     leftover = x - y
     return leftover > 0
 
-meal = int(input(ru.QUESTION_1)) # local in ()
-portions = int(input(ru.QUESTION_2)) # local in ()
-budget = int(input(ru.QUESTION_3)) # local in ()
-match meal:
-    case 1:
-        if broke(budget,cost) == True: print(f'{ru.EXPENSES} {meal} {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
-              f' {(broke(budget,cost))//PROTEIN_COST}')
-        else: print(f'{ru.LACK} {ru.ADD}')
 
+def main_function(meal, portions, budget):
+    match meal:
+        case 1:
+            if broke(budget,cost) == True: print(f'{ru.EXPENSES} {meal} {ru.RUBLS}\n {ru.PROSPERITY}\n {ru.BAR}'
+                  f' {(broke(budget,cost))//PROTEIN_COST}')
+            else: print(f'{ru.LACK} {ru.ADD}')
+
+
+if __name__ == "__main__":
+    meal = int(input(ru.QUESTION_1)) # local in ()
+    portions = int(input(ru.QUESTION_2)) # local in ()
+    budget = int(input(ru.QUESTION_3)) # local in ()
+    main_function(meal, portions, budget)
